8418faf35ac3aea9b8c909934a13922bdd3e281f
rubenavaldez <rubenavaldez@yahoo.com>
2022-06-18 13:01:32 -0700

    primitive data types

--- Tutorial/src/Tutorial/Main.java
+++ Tutorial/src/Tutorial/Main.java
@@ -4,8 +4,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		System.out.println("Hello World2");
+		
+//	Primitive data types - not changeable 
+		// integer whole number 			
+		int hello_world = 5; 
+		
+		// number with a decimal place 
+		double num2 = 5.0;
+		
+		// boolean true or false 
+		boolean b = false;
+		
+		// Char any single character
+		// can be quotes or Unix code 
+		char c = 'h';
+		
+		// String multiple characters in double quotes  
+		String str ="tim";
+		
+		
+		System.out.println(str);
+			
+		
+		
 	}
 
 }
